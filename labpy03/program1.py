e401d6fb6279d2d46b905668ee83e716a7bec940
Kudav5 <kdvalerian@gmail.com>
2022-11-05 21:02:24 +0700

    labpy03

---
+++ labpy03/program1.py
@@ -0,0 +1,18 @@
+'''
+2. Masukkan latihan1.py dan latihan2.py ke dalam repository.
+
+4. buat file README.md, yang berisi penjelasan alur algoritma program latihan1.py,
+latihan2.py, dan program1.py beserta screenshot hasilnya.
+'''
+a = float(input('Laba bulan ke-1 sebesar: '))
+b = float(input('Laba bulan ke-2 sebesar: '))
+c = float(input('Laba bulan ke-3 sebesar: '))
+d = float(input('Laba bulan ke-4 sebesar: '))
+e = float(input('Laba bulan ke-5 sebesar: '))
+f = float(input('Laba bulan ke-6 sebesar: '))
+g = float(input('Laba bulan ke-7 sebesar: '))
+h = float(input('Laba bulan ke-8 sebesar: '))
+
+
+i = a+b+c+d+e+f+g+h
+print('Total laba adalah : ', i)
