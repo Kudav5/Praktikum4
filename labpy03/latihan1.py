0a6d4dc02530d38b4480bc213b887e4fbba7f984
Kudav5 <kdvalerian@gmail.com>
2022-11-06 13:31:15 +0700

    modul praktikum2

--- labpy03/latihan1.py
+++
@@ -1,6 +0,0 @@
-import random
-
-a = int(input('Masukkan jumlah n : '))
-for i in range(a):
-    n = random.random()
-    print(n)
--- labpy03/latihan2.py
+++
@@ -1,8 +0,0 @@
-# modul 3
-while True:
-    a = int(input('Masukkan bilangan / 0: '))
-    if a == 0:
-        break
-    d = [a]
-d.sort()
-print('Urutan bilangan dari terkecil hingga terbesar: ',d)
