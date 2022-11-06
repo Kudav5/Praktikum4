00dbb88d8ce94a056977af6158861e6576091fce
Kudav5 <kdvalerian@gmail.com>
2022-11-06 13:29:20 +0700

    modul praktikum3

---
+++ modul praktikum3 (labpy03)/latihan2.py
@@ -0,0 +1,8 @@
+# modul 3
+while True:
+    a = int(input('Masukkan bilangan / 0: '))
+    if a == 0:
+        break
+    d = [a]
+d.sort()
+print('Urutan bilangan dari terkecil hingga terbesar: ',d)
